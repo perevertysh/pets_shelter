a7e0b7fcaf48f1bb6f9113f719593fe3237af6aa
junior <tmamedov1992@gmail.com>
2020-03-07 17:05:37 +0300

    profile

--- frontend/pages/pet_profile_page.vue
+++ frontend/pages/pet_profile_page.vue
@@ -1,21 +1,18 @@
 <template>
-    <b-container>
+    <b-container class="profile">
         <b-row>
             <b-col key='image' sm="4">
-                <pet-card 
-                    :item="item"
-                    :size='360'
-                    read-only
-                />
+                <b-img :src='item.photo' fluid rounded/>
             </b-col>
             <b-col key='info'>
                 <div>
-                    Вид животного: {{species}}<br/>
-                    Пол: {{item.sex}}<br/>
-                    Порода: {{breed}}<br/>
+                    Имя: {{item.name}}<br/>
+                    Вид животного: {{item.species.name}}<br/>
+                    Пол: {{item.gender.name}}<br/>
+                    Порода: {{item.breed.name}}<br/>
                     Возраст: {{item.age + " " + make_word_end()}}<br/>
-                    Регистрационный документ: {{doc}}<br/>
-                    Статус: {{item.status ? 'Приютили' : 'Не приютили'}}
+                    Регистрационный документ: {{item.doc.reg_num}}<br/>
+                    Статус: {{!item.status ? 'Не приютили' : item.status.name}}
                 </div>
                 <b-button class="mt-3" variant="primary" @click="shelter()">Приютить</b-button>
             </b-col>
@@ -28,7 +25,6 @@
 import rest from './../js/rest'
 import functions from './js/functions'
 import ShelterPet from './shelter_pet'
-import PetCard from './pet_card'
 
 export default {
     name: 'ProfilePet',
@@ -37,14 +33,10 @@ export default {
     ],
     components: {
         ShelterPet,
-        PetCard,
     },
     data: function() {
         return {
             item: {},
-            species: null,
-            breed: null,
-            doc: null,
         };
     },
     mounted: function() {
@@ -54,28 +46,10 @@ export default {
         loadData() {
             rest.pet.get(this.$route.params['id']).then(res => {
                 this.item = res.data;
-                this.loadDetailInfo();
             }).catch(err => {
                 console.error(err);
                 this.$router.go(-1);
             });
-        },
-        loadDetailInfo() {
-            this.clear();
-            rest.breed.get(this.item.breed).then(res => {
-                this.breed = res.data.name;
-            }).catch(err => console.error(err));
-            rest.species.get(this.item.species).then(res => {
-                this.species = res.data.name;
-            }).catch(err => console.error(err));
-            rest.doc.get(this.  item.doc).then(res => {
-                this.doc = res.data.reg_num;
-            }).catch(err => console.error(err));
-        },
-        clear() {
-            this.species = null;
-            this.breed = null;
-            this.doc = null;
         },
     }
 }
